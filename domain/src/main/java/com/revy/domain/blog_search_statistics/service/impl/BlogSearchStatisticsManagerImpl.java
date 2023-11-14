bf7893a51fb3ffe070e894f69d2d14def1e69d9f
Revy <wantedhooni@gmail.com>
2023-11-14 20:39:16 +0900

    REFACTOR: import 정리

--- domain/src/main/java/com/revy/domain/blog_search_statistics/service/impl/BlogSearchStatisticsManagerImpl.java
+++ domain/src/main/java/com/revy/domain/blog_search_statistics/service/impl/BlogSearchStatisticsManagerImpl.java
@@ -9,8 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 /**
  * Created by Revy on 2023.11.12
  */
